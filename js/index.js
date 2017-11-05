a59dc4efb111689ebf507a63f642b241764adc79
Ikram Qabiyev <ikram20011127@gmail.com>
2017-11-05 13:35:34 +0400

    complete home page

--- js/index.js
+++ js/index.js
@@ -16,3 +16,31 @@ $(".navSlideBtn").click(function(){
         checkNavStatus = false;
     }
 });
+// *************
+// slider
+// *************
+
+$(document).ready(function(){
+    var frameHeight = $(".sliderFrame").height();
+    $(".ikramMainSlider>.container>.sliderFrame>button").css("top",frameHeight/2.6 + "px")
+})
+$(window).resize(function(){
+    var frameHeight = $(".sliderFrame").height();
+    $(".ikramMainSlider>.container>.sliderFrame>button").css("top",frameHeight/2.6 + "px")
+})
+
+var leftPos = 0;
+$(".ikramMainSlider>.container>.sliderFrame>.leftSliderBtn").click(function(){
+    leftPos+=100
+    if(leftPos == 100){
+        leftPos = -200
+    }
+    $(".ikramMainSlider>.container>.sliderFrame>.sliderLent").css("left",leftPos+"%")
+})
+$(".ikramMainSlider>.container>.sliderFrame>.rightSliderBtn").click(function(){
+    leftPos-=100
+    if(leftPos == -300){
+        leftPos = 0
+    }
+    $(".ikramMainSlider>.container>.sliderFrame>.sliderLent").css("left",leftPos+"%")
+})
